3720f8959f00b3a828f47a3b2a50c68b225c0d73
Roman <igoshinr.estho@gmail.com>
2022-07-18 18:33:47 +0300

    015

---
+++ 015/Program.cs
@@ -0,0 +1,4 @@
+﻿int day = Convert.ToInt32(Console.ReadLine());
+while (day < 1 || day > 7) day = Convert.ToInt32(Console.ReadLine());
+if (day <= 5) Console.WriteLine("нет");
+else Console.WriteLine("да");
